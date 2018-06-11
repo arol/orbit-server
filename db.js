e8ba6994d504d5dfa3c7cadd47937057ba2c3235
Arol <arolet@gmail.com>
2018-06-11 21:08:03 +0200

    style: remove comment

--- db.js
+++ db.js
@@ -28,16 +28,4 @@ mongoose.connect(mongodbUri, options)
   })
   .catch(err => {
     console.log('Connection error', err);
-  })
-
-
-// CONNECTING TO LOCAL DATABASE
-// const mongoose = require('mongoose');
-//
-// mongoose.connect('mongodb://localhost/orbit-database')
-//   .then(() => {
-//     console.log('Connected to Orbits database');
-//   })
-//   .catch(err => {
-//     console.log('Cannot connect, wrong path');
-//   })
+  });
